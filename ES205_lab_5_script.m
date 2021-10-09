5012729294c8cbe01779ae631fc81a3aabfbb028
Evan Schimmel <evanrschimmel@gmail.com>
2021-10-09 13:06:33 -0400

    Make sim ICs equal to exp ICs
    And some plotting options

--- ES205_lab_5_script.m
+++ ES205_lab_5_script.m
@@ -15,30 +15,6 @@ fit2 = polyfit(hcdata,vcdata2, 1);
 m2 = fit2(1);
 int2 = fit2(2);
 
-%% Define constants and variables
-
-g = 32.2 * 12; % in/s^2
-
-% Tank 1
-Atank1 = 22.94; % cross sectional area of tank, in^2
-d1 = 0.335; % diameter of orfice, in
-Aout1 = pi * (d1/2)^2; % cross sectional area of exit orfice, in^2
-a1 = 3.49; % outlet length, in
-b1 = 0.5; % outlet inset, in
-Cd1_orig = 0.7; % discharge coefficient, unitless
-h1_0_single = 9; % initial water height, in
-h1_0_double = 8; % initial water height, in
-
-% Tank 2
-Atank2 = 22.94; % cross sectional area of tank, in^2
-d2 = 0.240; % diameter of orfice, in
-Aout2 = pi * (d2/2)^2; % cross sectional area of exit orfice, in^2
-a2 = 3.24; % outlet length, in
-b2 = 0.5; % outlet inset, in
-Cd2_orig = 0.7; % discharge coefficient, unitless
-h2_0_single = 9; % initial water height, in
-h2_0_double = 4; % initial water height, in
-
 %% Pull experimental data and sort into vectors, run sim with original CDs
 
 upper_data_single = readmatrix('ES205_Lab_05_Upper_Tank');
@@ -54,16 +30,42 @@ h2data_single = (((lower_data_single(:,3)-int2)) ./ m2);
 h1data_double = (((data_double(:,2)-int1)) ./ m1);
 h2data_double = (((data_double(:,3)-int2)) ./ m2);
 
+%% Define constants and variables
+
+g = 32.2 * 12; % in/s^2
+
+% Tank 1
+Atank1 = 22.94; % cross sectional area of tank, in^2
+d1 = 0.335; % diameter of orfice, in
+Aout1 = pi * (d1/2)^2; % cross sectional area of exit orfice, in^2
+a1 = 3.49; % outlet length, in
+b1 = 0.5; % outlet inset, in
+Cd1_orig = 0.7; % discharge coefficient, unitless
+
+% Tank 2
+Atank2 = 22.94; % cross sectional area of tank, in^2
+d2 = 0.240; % diameter of orfice, in
+Aout2 = pi * (d2/2)^2; % cross sectional area of exit orfice, in^2
+a2 = 3.24; % outlet length, in
+b2 = 0.5; % outlet inset, in
+Cd2_orig = 0.7; % discharge coefficient, unitless
+
 maxstep = 0.01; % max step time, s
 tol = 1e-6;
 
 tf_upper = tdata_upper(end);
+h1_0_single = h1data_single(1); % initial water height, in
 sim('ES205_lab_5_upper_model_orig')
+see_upper_orig = sqrt(sum((h1_single_orig-h1data_single).^2)./(length(h1data_single)-2));
 
 tf_lower = tdata_lower(end);
+h2_0_single = h2data_single(1); % initial water height, in
 sim('ES205_lab_5_lower_model_orig')
+see_lower_orig = sqrt(sum((h2_single_orig-h2data_single).^2)./(length(h2data_single)-2));
 
 tf_double = tdata_double(end);
+h1_0_double = h1data_double(1); % initial water height, in
+h2_0_double = h2data_double(2); % initial water height, in
 sim('ES205_lab_5_dual_model_orig')
 
 %% Run loop for best Cd
@@ -73,44 +75,46 @@ dex = 1;
 for Cd1_mod = 0.5:stepsize:1
     sim('ES205_lab_5_upper_model_mod')
 values1(dex)=Cd1_mod;
-see(dex)= sqrt(sum((h1_single_mod-h1data_single).^2)./(length(h1data_single)-2));
+see(dex) = sqrt(sum((h1_single_mod-h1data_single).^2)./(length(h1data_single)-2));
 dex = dex+1;
 end
 [~,si]= min(see,[],'linear');
 Cd1_mod=values1(si);
 sim('ES205_lab_5_upper_model_mod')
+see_upper_mod = sqrt(sum((h1_single_mod-h1data_single).^2)./(length(h1data_single)-2));
 
 dex = 1;
 for Cd2_mod = 0.5:stepsize:1
     sim('ES205_lab_5_lower_model_mod')
 values2(dex)=Cd2_mod;
-see(dex)= sqrt(sum((h2_single_mod-h2data_single).^2)./(length(h2data_single)-2));
+see(dex) = sqrt(sum((h2_single_mod-h2data_single).^2)./(length(h2data_single)-2));
 dex = dex+1;
 end
-[~,si]= min(see,[],'linear');
-Cd2_mod=values2(si);
+[~,si] = min(see,[],'linear');
+Cd2_mod = values2(si);
 sim('ES205_lab_5_lower_model_mod')
+see_lower_mod = sqrt(sum((h2_single_mod-h2data_single).^2)./(length(h2data_single)-2));
 
 sim('ES205_lab_5_dual_model_mod')
 
 %% Plot
 
 figure
-plot(tdata_upper,h1data_single,'k-',t_upper,h1_single_orig,'r--',t_upper,h1_single_mod,'b--')
+plot(tdata_upper,h1data_single,'b-',t_upper,h1_single_orig,'b:',t_upper,h1_single_mod,'b--')
 xlabel('Time [s]')
 ylabel('Water height [in]')
 legend('Experimental upper tank','Original upper tank model','Refined upper tank model')
 set(gcf, 'color', 'w')
 
 figure
-plot(tdata_lower,h2data_single,'k-',t_lower,h2_single_orig,'r--',t_lower,h2_single_mod,'b--')
+plot(tdata_lower,h2data_single,'r-',t_lower,h2_single_orig,'r:',t_lower,h2_single_mod,'r--')
 xlabel('Time [s]')
 ylabel('Water height [in]')
 legend('Experimental lower tank','Original lower tank model','Refined lower tank model')
 set(gcf, 'color', 'w')
 
 figure
-plot(tdata_double,h1data_double,'k-',tdata_double,h2data_double,'k-',t_double,h1_double_orig,'r--',t_double,h1_double_mod,'b--',t_double,h2_double_orig,'r--',t_double,h2_double_mod,'b--')
+plot(tdata_double,h1data_double,'b-',tdata_double,h2data_double,'r-',t_double,h1_double_orig,'b:',t_double,h1_double_mod,'b--',t_double,h2_double_orig,'r:',t_double,h2_double_mod,'r--')
 xlabel('Time [s]')
 ylabel('Water height [in]')
 legend('Experimental upper tank','Experimental lower tank','Original upper tank model','Refined upper tank model','Original lower tank model','Refined lower tank model')
